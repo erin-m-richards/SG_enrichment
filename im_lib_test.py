28e85d11cb70f5936ae1cb1705fc8da18433b9fe
erinmrichards <erin_richards@rocketmail.com>
2021-11-10 20:53:26 +0000

    Outline of test cases for Erin's portion of image manipulation library.

---
+++ im_lib_test.py
@@ -0,0 +1,101 @@
+import unittest
+import im_lib
+
+
+class ReadImageTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(clc):
+        print("\nRunning ReadImage class setUp...")
+
+    @classmethod
+    def tearDownClass(clc):
+        print("\nRunning ReadImage class tearDown...")
+
+    def setUp(self):
+        print("\nRunning setUp...")
+
+    def tearDown(self):
+        print("\nRunning tearDown...")
+    
+    def test_ri(self):
+    
+
+class DetectCellsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(clc):
+        print("\nRunning DetectCells class setUp...")
+
+    @classmethod
+    def tearDownClass(clc):
+        print("\nRunning DetectCells class tearDown...")
+
+    def setUp(self):
+        print("\nRunning setUp...")
+
+    def tearDown(self):
+        print("\nRunning tearDown...")
+        
+    def test_dc(self):
+    
+    
+class FindObjectsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(clc):
+        print("\nRunning FindObjects class setUp...")
+
+    @classmethod
+    def tearDownClass(clc):
+        print("\nRunning FindObjects class tearDown...")
+
+    def setUp(self):
+        print("\nRunning setUp...")
+
+    def tearDown(self):
+        print("\nRunning tearDown...")
+        
+    def test_fobj(self):
+
+        
+class FindOverlapTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(clc):
+        print("\nRunning FindOverlap class setUp...")
+
+    @classmethod
+    def tearDownClass(clc):
+        print("\nRunning FindOverlap class tearDown...")
+
+    def setUp(self):
+        print("\nRunning setUp...")
+
+    def tearDown(self):
+        print("\nRunning tearDown...")
+        
+    def test_foverlap(self):
+    
+
+class CountObjectsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(clc):
+        print("\nRunning CountObjects class setUp...")
+
+    @classmethod
+    def tearDownClass(clc):
+        print("\nRunning CountObjects class tearDown...")
+
+    def setUp(self):
+        print("\nRunning setUp...")
+
+    def tearDown(self):
+        print("\nRunning tearDown...")
+        
+    def test_co(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
