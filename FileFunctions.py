388adf5879c789e41fe25c05dbe8b7174194932d
Rgarris <rgarris1298@gmail.com>
2021-11-15 21:24:52 +0000

    reformatt and docstring

--- FileFunctions.py
+++ FileFunctions.py
@@ -1,6 +1,6 @@
-import os, shutil
-import sys
-import argparse, configparser 
+import os, shutil  # for maniplulating files
+import sys  # for exit codes
+import argparse, configparser  # for manipulating inputs
 
 
 def input_parser():
@@ -29,9 +29,9 @@ def input_parser():
     args_parsed = my_parser.parse_args()
     
     # handle if no config file listed
-    if args_parsed.config == None:
-        raise TypeError\
-        (f" No config file was listed. Please specify a config file using '-c'.")
+    if args_parsed.config is None:
+        raise TypeError(f" No config file was listed. Please specify a config" 
+                        f"file using '-c'.")
         sys.exit(1)
     
     # handle if the given directory does not exist
@@ -39,24 +39,39 @@ def input_parser():
         config_file = configparser.ConfigParser()
         config_file.read(args_parsed.config)
     except FileNotFoundError:
-        print(f"The config file that you listed {args_parsed.config=} could not be found.")
+        print(f"The config file that you listed {args_parsed.config=} could "
+              f"not be found.")
         sys.exit(1)
     
     # generate dictionary with all of the necessary inputs
     inputs = {
-        'image_directory' : config_file['FILE LOCATIONS']['image_directory'],
-        'out_put_location' : config_file['FILE LOCATIONS']['outputs_directory'],
-        'experiment_name' : config_file['EXPERIMENT INFO']['experiment_name'],
-        'C1' : config_file['EXPERIMENT INFO']['C1'],
-        'C2' : config_file['EXPERIMENT INFO']['C2'],
-        'C3' : config_file['EXPERIMENT INFO']['C3'],
-        'num_groups' : int(config_file['EXPERIMENT INFO']['num_groups']),
-        'group_names' : [config_file['EXPERIMENT INFO']['group_names']]
+        'image_directory': config_file['FILE LOCATIONS']['image_directory'],
+        'out_put_location': config_file['FILE LOCATIONS']['outputs_directory'],
+        'experiment_name': config_file['EXPERIMENT INFO']['experiment_name'],
+        'C1': config_file['EXPERIMENT INFO']['C1'],
+        'C2': config_file['EXPERIMENT INFO']['C2'],
+        'C3': config_file['EXPERIMENT INFO']['C3'],
+        'num_groups': int(config_file['EXPERIMENT INFO']['num_groups']),
+        'group_names': [config_file['EXPERIMENT INFO']['group_names']]
     }
     return inputs
 
 
 def sort_images(directory):
+    """
+    PARAMETERS
+    ----------
+    directory: str
+        This is a string that has the full file path to the directory 
+        containing all of the images to be analyzed. Format will change 
+        based on operating system.
+    
+    RETURNS
+    ----------
+    full_paths: list
+        This list contains strings with the complete file paths of the three
+        folders containing images from each seperate channel.
+    """
     # check that the directory exists
     try: 
         os.chdir(directory)
@@ -65,7 +80,6 @@ def sort_images(directory):
         sys.exit(1)
     
     # remove non tif files from directory
-    #files = os.listdir(directory)
     files = [f for f in os.listdir(directory) if not f.startswith('.')]
     removed_files = []
     for file in files:
@@ -74,12 +88,14 @@ def sort_images(directory):
             pass
         else:
             removed_files.append(file)
-    if len(removed_files)==0:
+    if len(removed_files) == 0:
         print(f"No files were removed from this directory: {directory}")
     elif len(removed_files) != 0:
-        print(f"Some of the files in this directory were removed because they were not the right file type, they can be found in the 'removed_files' folder: n/{removed_files}")
+        print(f"Some of the files in this directory were removed because they"
+              f" were not the right file type, they can be found in the "
+              f"'removed_files' folder: n/{removed_files}")
     
-    #sort images by channel
+    # sort images by channel
     C1_images = []
     C2_images = []
     C3_images = []
@@ -94,32 +110,37 @@ def sort_images(directory):
             removed_files.append(file)
 
     # make directories for each type of image
-    sub_dirs = ['C1', 'C2','C3','removed_files']
+    sub_dirs = ['C1', 'C2', 'C3', 'removed_files']
     full_paths = []
     for folder in sub_dirs:
-        folder = os.path.join(directory,folder)
+        folder = os.path.join(directory, folder)
         os.mkdir(folder)
         full_paths.append(folder)
         
     # move all files to their respective sub folders
     for file in C1_images:
-        shutil.move(os.path.join(directory, file), os.path.join(full_paths[0], file))
+        shutil.move(os.path.join(directory, file), 
+                    os.path.join(full_paths[0], file)
+                    )
     for file in C2_images: 
-        shutil.move(os.path.join(directory, file), os.path.join(full_paths[1], file))  
+        shutil.move(os.path.join(directory, file), 
+                    os.path.join(full_paths[1], file)
+                    )  
     for file in C3_images: 
-        shutil.move(os.path.join(directory, file), os.path.join(full_paths[2], file))
+        shutil.move(os.path.join(directory, file), 
+                    os.path.join(full_paths[2], file)
+                    )
     for file in removed_files: 
-        shutil.move(os.path.join(directory, file), os.path.join(full_paths[3], file))
+        shutil.move(os.path.join(directory, file), 
+                    os.path.join(full_paths[3], file)
+                    )
         
     # get full file paths to return for only C1, C2 and C3
     del full_paths[3]
+    
     return full_paths
 
-
-        
-        
+       
 # testing
 inputs = input_parser()
 sort_images(inputs['image_directory']) 
-
-
